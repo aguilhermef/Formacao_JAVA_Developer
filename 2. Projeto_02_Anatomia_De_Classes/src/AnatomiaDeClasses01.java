052e59b50caf38c18bbe43464c6bf948a0e21e82
aguilhermef <guilhermefelicianoalves@gmail.com>
2023-08-21 19:18:40 -0300

    1. Anatomia_De_Classes

---
+++ 2. Projeto_02_Anatomia_De_Classes/src/AnatomiaDeClasses01.java
@@ -0,0 +1,5 @@
+public class AnatomiaDeClasses01 {
+    public static void main(String[] args) {
+        System.out.print ("Olá, eu sou o Gui! Essa é a minha primeira classe em JAVA, com um método que é executável.");
+    }
+}
